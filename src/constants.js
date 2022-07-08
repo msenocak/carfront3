369fe19300bf47cdc6a00d9633e4e95e24a83981
msenocak <msenocak3@gmail.com>
2022-07-08 12:40:21 -0700

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+//export const SERVER_URL='http://localhost:8080/';
+//export const SERVER_URL='https://mscarbackend.herokuapp.com/';
+export const SERVER_URL='http://ch154cardatabasemariadb-env.eba-37ypzp3m.us-west-1.elasticbeanstalk.com/';
